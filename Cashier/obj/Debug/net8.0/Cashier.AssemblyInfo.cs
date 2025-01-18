3b9948871b7526941044476a9e7c06157baa21e5
Dobozi Andras <dbzmonty@gmail.com>
2025-01-18 20:40:18 +0100

    Bootstrap added and categories populated from model

--- Cashier/obj/Debug/net8.0/Cashier.AssemblyInfo.cs
+++ Cashier/obj/Debug/net8.0/Cashier.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cashier")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f44dac4894c6b3717ef2bb50f228c70f05fb78cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c647b79fda7d022f8c2fb9909a85a20d8f4e3161")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cashier")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cashier")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
